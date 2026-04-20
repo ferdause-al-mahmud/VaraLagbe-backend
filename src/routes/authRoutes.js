df686176501ac0d9de520a4a7f0b6c9774ff5018
Mahin Ahmed  Dipta <123046479+diptomahin@users.noreply.github.com>
2026-04-20 09:25:34 +0000

    added login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signUp } = require('../controllers/authController');
+const { login, signUp } = require('../controllers/authController');
 
+router.post('/login', login);
 router.post('/signup', signUp);
 router.post('/register', signUp);
 
